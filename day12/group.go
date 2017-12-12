4adcb5c35190e97b3f0f9cd41e443460b366e858
basverweij <bas@off-sync.com>
2017-12-12 19:36:39 +0100

    Completed day 12, puzzle 2.

--- day12/group.go
+++ day12/group.go
@@ -1,6 +1,6 @@
 package main
 
-func groupSize(programs map[int][]int, program int) int {
+func group(programs map[int][]int, program int) map[int]struct{} {
 	seen := make(map[int]struct{}, len(programs))
 	todo := make([]int, 1)
 	todo[0] = program
@@ -22,5 +22,26 @@ func groupSize(programs map[int][]int, program int) int {
 		seen[p] = struct{}{}
 	}
 
-	return len(seen)
+	return seen
+}
+
+func groupCount(programs map[int][]int) int {
+	seen := make(map[int]struct{}, len(programs))
+
+	groupCount := 0
+	for id := range programs {
+		if _, found := seen[id]; found {
+			// part of an already counted group
+			continue
+		}
+
+		g := group(programs, id)
+		for id := range g {
+			seen[id] = struct{}{}
+		}
+
+		groupCount++
+	}
+
+	return groupCount
 }
--- day12/main.go
+++ day12/main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Puzzle 1: %d\n", puzzle1())
+
+	fmt.Printf("Puzzle 2: %d\n", puzzle2())
 }
 
 func puzzle1() int {
-	return groupSize(input, 0)
+	return len(group(input, 0))
+}
+
+func puzzle2() int {
+	return groupCount(input)
 }
